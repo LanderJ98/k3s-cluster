bea53c2046017c8f00b67ae76d85c827d6bfc40e
Josie Lander <landerjosie@gmail.com>
2022-10-22 19:15:11 +0100

    Use pathexpand function

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -20,7 +20,7 @@ terraform {
 provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
-  private_key_path = file(var.private_key_path)
+  private_key_path = pathexpand(var.private_key_path)
   fingerprint      = var.fingerprint
   region           = "uk-london-1"
 }
